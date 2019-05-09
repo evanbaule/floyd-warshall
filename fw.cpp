7a63acf5c8ceae6f9c94c91b9af7f34b08220b32
Evan Baule <evanbaule@Evans-MacBook-Pro-3.local>
2019-05-09 13:16:44 -0400

    reformatted printing

--- fw.cpp
+++ fw.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 // returns encapsulated results for each test case
 result*
-floyd_warshall(int v, int edge_chance, int i)
+floyd_warshall(int v, int edge_chance, unsigned long long seed, bool p)
 {
     result* res = new result(); //encapsulated results
 
@@ -27,7 +27,6 @@ floyd_warshall(int v, int edge_chance, int i)
     int generated;
     int weight;
 
-    unsigned long long seed = (i*i) ^ std::chrono::system_clock::now().time_since_epoch().count();
     srand(seed);
     //#pragma omp parallel for
     for(int i = 0; i < v; i++)
@@ -36,7 +35,7 @@ floyd_warshall(int v, int edge_chance, int i)
         for(int j = 0; j < v; j++)
         {
             generated = rand() % 99;
-            weight = (rand() % 8) - 1;
+            weight = (rand() % 8);
             if (i == j)
             {
                 g[i][j] = 0;
@@ -56,22 +55,27 @@ floyd_warshall(int v, int edge_chance, int i)
     }
 
     // Print out the original matrix
-    for(int i = 0; i < v; i++)
+    if(p)
     {
-        for(int j = 0; j < v; j++)
+        std::cout << "------------------------------------------------------" << std::endl;
+        std::cout << "Randomized Matrix: " << std::endl;
+        for(int i = 0; i < v; i++)
         {
-            if(g[i][j] < INF - 100)
+            for(int j = 0; j < v; j++)
             {
-                cout << g[i][j] << ",";
+                if(g[i][j] < INF - 100)
+                {
+                    std::cout << "{ " << g[i][j] << " } ";
+                }
+                else
+                {
+                    std::cout << "{INF} ";
+                }
             }
-            else
-            {
-                cout << "INF" << ",";
-            }
+            cout << endl;
         }
-        cout << endl;
     }
-    std::cout << "------------------------------------------------------" << std::endl;
+
 
     // V[k] = node to be considered as intermediary
     for (int k = 0; k < v; k++)
@@ -106,32 +110,38 @@ floyd_warshall(int v, int edge_chance, int i)
     }
 
     // Print out the resulting matrix
-    for(int i = 0; i < v; i++)
+    if(p)
     {
-        for(int j = 0; j < v; j++)
+        std::cout << "------------------------------------------------------" << std::endl;
+        std::cout << "Path Length Matrix: " << std::endl;
+        for(int i = 0; i < v; i++)
         {
-            if(g[i][j] < INF - 100)
+            for(int j = 0; j < v; j++)
             {
-                cout << g[i][j] << ",";
+                if(g[i][j] < INF - 100)
+                {
+                    std::cout << "{ " << g[i][j] << " } ";
+                }
+                else
+                {
+                    std::cout << "{INF} ";
+                }
             }
-            else
+            std::cout << std::endl;
+        } 
+    
+        std::cout << "------------------------------------------------------" << std::endl;
+        std::cout << "Path Matrix: " << std::endl;
+        // Print out the path matrix
+        for(int i = 0; i < v; i++)
+        {
+            for(int j = 0; j < v; j++)
             {
-                cout << "INF" << ",";
+                std::cout << "{ " << paths[i][j] << " } ";
             }
+            std::cout << std::endl;
         }
-        cout << endl;
-    }
 
-    std::cout << "------------------------------------------------------" << std::endl;
-    std::cout << "Path matrix: " << std::endl;
-    // Print out the path matrix
-    for(int i = 0; i < v; i++)
-    {
-        for(int j = 0; j < v; j++)
-        {
-            std::cout << "{ " << paths[i][j] << " } ";
-        }
-        std::cout << std::endl;
     }
 
     //encapsulated results from computation
@@ -205,67 +215,98 @@ main(int argc, char** argv)
     //int num_tests = 1'000;
     //int num_tests = 10;
 
-    std::cout << " Simulation Start: " << std::endl;
+    // std::cout << " Simulation Start: " << std::endl;
 
-    //Generate results for invalid graph generation
-    int invalid = 0;
-    int invalid_results_25 = 0;
-    int invalid_results_50 = 0;
-    int invalid_results_75 = 0;
-    int invalid_results_100 = 0;
-    //result* _res = new result();
+    // //Generate results for invalid graph generation
+    // int invalid = 0;
+    // int invalid_results_25 = 0;
+    // int invalid_results_50 = 0;
+    // int invalid_results_75 = 0;
+    // int invalid_results_100 = 0;
+    // //result* _res = new result();
 
-    std::cout << "Graph size: " <<  v << " * " << v << std::endl;
-    int num_tests = 100'000;
-    for(int i = 0; i < num_tests; i++)
+    // std::cout << "Graph size: " <<  v << " * " << v << std::endl;
+    // int num_tests = 100'000;
+    // for(int i = 0; i < num_tests; i++)
+    // {
+    //     //std::cout << "Test Number: " << j << std::endl;
+    //     if(detect_neg_floyd_warshall(v, 25, 25*i)) invalid_results_25++;
+    //     if(detect_neg_floyd_warshall(v, 50, 50*i)) invalid_results_50++;
+    //     if(detect_neg_floyd_warshall(v, 75, 75*i)) invalid_results_75++;
+    //     if(detect_neg_floyd_warshall(v, 100, i)) invalid_results_100++;
+    // }
+
+    // std::cout << "Results for neg cycle detection: " << std::endl;
+
+    // float perc = ((float) invalid_results_25 / num_tests) * 100;
+    // std::cout << "  - .25 Density Graphs: " << std::setprecision(4) << perc << "% generated are invalid." << std::endl;
+
+    // perc = ((float) invalid_results_50 / num_tests) * 100;
+    // std::cout << "  - .50 Density Graphs: " << std::setprecision(4) << perc << "% generated are invalid." << std::endl;
+
+    // perc = ((float) invalid_results_75 / num_tests) * 100;
+    // std::cout << "  - .75 Density Graphs: " << std::setprecision(4) << perc << "% generated are invalid." << std::endl;
+    
+    // perc = ((float) invalid_results_100 / num_tests) * 100;
+    // std::cout << "  - 1.00 Density Graphs: " << std::setprecision(4) << perc << "% generated are invalid." << std::endl;
+
+    // std::cout << "--------------------------------------------------------------------------" << std::endl;
+    // std::cout << "--------------------------------------------------------------------------" << std::endl;
+    // std::cout << "--------------------------------------------------------------------------" << std::endl;
+    // auto start_outer = std::chrono::system_clock::now();
+    
+    unsigned long long seed;
+    for(int i = 0; i < 100; i++)
     {
-        //std::cout << "Test Number: " << j << std::endl;
-        if(detect_neg_floyd_warshall(v, 25, 25*i)) invalid_results_25++;
-        if(detect_neg_floyd_warshall(v, 50, 50*i)) invalid_results_50++;
-        if(detect_neg_floyd_warshall(v, 75, 75*i)) invalid_results_75++;
-        if(detect_neg_floyd_warshall(v, 100, i)) invalid_results_100++;
+        seed = (i*i*i*i) & ((std::chrono::system_clock::now().time_since_epoch().count()));
+        if(dev_print)
+        {
+            std::cout << "------------------------------------------------------" << std::endl;
+            std::cout << "----------------------- i: " << i << " ------------------------" << std::endl;
+            std::cout << "------------------------------------------------------" << std::endl;
+            std::cout << "Random Generator Seed: " << seed << std::endl;
+        }
+
+        //run algorithm
+        floyd_warshall(5, 35, seed, false);
     }
 
-    std::cout << "Results for neg cycle detection: " << std::endl;
+    // auto end_outer = std::chrono::system_clock::now();
+    // double tdiff_outer = std::chrono::duration<double>(end_outer-start_outer).count();
+    // std::cout << "Total time taken for " << num_tests << " graphs: " << std::setprecision(4) << tdiff_outer << " ns" << std::endl;
 
-    float perc = ((float) invalid_results_25 / num_tests) * 100;
-    std::cout << "  - .25 Density Graphs: " << std::setprecision(4) << perc << "% generated are invalid." << std::endl;
-
-    perc = ((float) invalid_results_50 / num_tests) * 100;
-    std::cout << "  - .50 Density Graphs: " << std::setprecision(4) << perc << "% generated are invalid." << std::endl;
-
-    perc = ((float) invalid_results_75 / num_tests) * 100;
-    std::cout << "  - .75 Density Graphs: " << std::setprecision(4) << perc << "% generated are invalid." << std::endl;
-    
-    perc = ((float) invalid_results_100 / num_tests) * 100;
-    std::cout << "  - 1.00 Density Graphs: " << std::setprecision(4) << perc << "% generated are invalid." << std::endl;
-
-    std::cout << "--------------------------------------------------------------------------" << std::endl;
-    std::cout << "--------------------------------------------------------------------------" << std::endl;
-    std::cout << "--------------------------------------------------------------------------" << std::endl;
-    auto start_outer = std::chrono::system_clock::now();
-    
-    for(int i = 0; i < num_tests; i++)
-    {
-        floyd_warshall(v, w, i);
-    }
-
-    auto end_outer = std::chrono::system_clock::now();
-    double tdiff_outer = std::chrono::duration<double>(end_outer-start_outer).count();
-    std::cout << "Total time taken for " << num_tests << " graphs: " << std::setprecision(4) << tdiff_outer << " ns" << std::endl;
-
-    std::cout << "--------------------------------------------------------------------------" << std::endl;
+    std::cout << "------------------------------------------------------" << std::endl;
 
     /*
-    @TODO:
-        Work on minimizing the probability of our dataset containing a shitty graph
-            Maybe no negative weights for the actual timing dataset
-        
-        Write timing code for avg time taken per graph (at 100x100 we should have a decent amt of time difference or something)
-        Make graphs to show data
+        Force Spawn Specific Matrices known to have demonstrable results
     */
+    std::vector<unsigned long long> seeds = 
+    { 
+        62135695,
+        22382654,
+        98039086,
+        2710561,
+        6948897,
+        659456,
+        2670848
+    };
+    for(unsigned int i = 0; i < seeds.size(); i++)
+    {
+        
+        std::cout << "------------------------------------------------------" << std::endl;
+        std::cout << "----------------------- i: " << i << " ------------------------" << std::endl;
+        std::cout << "------------------------------------------------------" << std::endl;
 
+        //try to get as 'random' seed as possible
+        seed = seeds[i];
 
-    std::cout << "goodbye " << std::endl;
+        //print seed given
+        std::cout << "Random Generator Seed: " << seed << std::endl;
 
+        //run algorithm
+        floyd_warshall(5, 35, seed, true);
+    }
+    std::cout << "------------------------------------------------------" << std::endl;
+
+    std::cout << "goodbye" << std::endl;
 }
--- fw.hpp
+++ fw.hpp
@@ -15,12 +15,13 @@ typedef struct result
 } result;
 
 static int INF = 99999;
+constexpr bool dev_print = false;
 constexpr int vexp = 2;
 static unsigned long e_seed = 923901283 & std::chrono::system_clock::now().time_since_epoch().count();
 static unsigned long w_seed = 212144421 ^ std::chrono::system_clock::now().time_since_epoch().count();
 
 result*
-floyd_warshall(int v, int edge_chance, int i);
+floyd_warshall(int v, int edge_chance, unsigned long long seed, bool p);
 
 inline bool
 detect_neg_floyd_warshall(int v, int edge_chance, int i);
